4700d2f79eaf23952aa7b065340ee9ced508f4b6
Melissa Kathryn Quinnan <melissa.quinnan@cern.ch>
2023-08-15 19:03:22 -0700

    bug fix

--- run_mass_regression_melissa.sh
+++ run_mass_regression_melissa.sh
@@ -16,7 +16,7 @@ python -u weaver/train.py \
  --steps-per-epoch=1 \
  --tensorboard _v10_load \
  --load-model-weights finetune_gghww_custom \
- --disco-lambda 100.0 \
+ --discolambda 100.0 \
  --discovar fj_ParT_inclusive_score \
  
 mv runs/* /fmhwwvol/runs_melissa/

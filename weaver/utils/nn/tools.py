1a1f1263caf0e4aeb9f844af9fa305fc9dbebca5
Melissa Quinnan <mkq5008@gmail.com>
2023-08-28 17:53:28 -0700

    Update tools.py

--- weaver/utils/nn/tools.py
+++ weaver/utils/nn/tools.py
@@ -303,11 +303,8 @@ def train_regression(model, loss_func, opt, scheduler, train_loader, dev, epoch,
                 preds = model_output.squeeze()
                 if discokey is not None:
                     print('DISCOVAR')
-                    print(X.keys())
                     print(data_config.disco_name)
-                    #print(X[data_config.disco_name[0]])
-                    #print(y[data_config.disco_name[0]])
-                    print(_[data_config.disco_name[0]])
+                    print discokey[data_config.disco_name)] #is now val_loader
                     #for k, v in _.items():
                        # observers[k].append(v.cpu().numpy())
                     discovar = _[discokey]
